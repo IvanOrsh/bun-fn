22821a039c2b5ef18f0e9511bfe8790845a9609b
IvanOrsh <ksironov@mail.ru>
2024-02-20 00:24:36 +0300

    feat: implement insert instance method for binary tree

--- src/11-binary-search-tree/03-level-order-traversal/level-order-traversal.spec.ts
+++ src/11-binary-search-tree/03-level-order-traversal/level-order-traversal.spec.ts
@@ -3,13 +3,61 @@ import { describe, it, expect } from "bun:test";
 import { BinaryTree } from "./level-order-traversal";
 
 describe("Test level order traversal", () => {
-  it.each([
-    {
-      arr: [7, 11, 1, null, 7, 2, 8, null, null, null, 3, null, null, 5, null],
-      result: [[7], [11, 2], [1, 8, 3], [7, 5]],
-    },
-  ])("$arr should return $result", ({ arr, result }) => {
-    const tree = new BinaryTree();
-    tree.insert(arr);
+  describe("insert", () => {
+    it("should insert nodes", () => {
+      const tree = new BinaryTree();
+      tree.insert([1, 2, 3, 4, 5]);
+
+      expect(tree.root?.value).toBe(1);
+      expect(tree.root?.left?.value).toBe(2);
+      expect(tree.root?.right?.value).toBe(3);
+      expect(tree.root?.left?.left?.value).toBe(4);
+      expect(tree.root?.left?.right?.value).toBe(5);
+    });
+
+    it("should insert empty array", () => {
+      const tree = new BinaryTree();
+      tree.insert([]);
+
+      expect(tree.root?.value).toBeUndefined();
+    });
+
+    it("should insert array that has some nulls", () => {
+      const tree = new BinaryTree();
+      tree.insert([1, 2, 3, null, 4]);
+
+      expect(tree.root?.value).toBe(1);
+      expect(tree.root?.left?.value).toBe(2);
+      expect(tree.root?.right?.value).toBe(3);
+      expect(tree.root?.left?.right?.value).toBe(4);
+    });
+  });
+
+  describe("test level order traversal", () => {
+    it.each([
+      {
+        arr: [
+          7,
+          11,
+          1,
+          null,
+          7,
+          2,
+          8,
+          null,
+          null,
+          null,
+          3,
+          null,
+          null,
+          5,
+          null,
+        ],
+        result: [[7], [11, 2], [1, 8, 3], [7, 5]],
+      },
+    ])("$arr should return $result", ({ arr, result }) => {
+      const tree = new BinaryTree();
+      tree.insert(arr);
+    });
   });
 });
--- src/11-binary-search-tree/03-level-order-traversal/level-order-traversal.ts
+++ src/11-binary-search-tree/03-level-order-traversal/level-order-traversal.ts
@@ -28,5 +28,54 @@ class Node<T> {
 export class BinaryTree<T> {
   constructor(public root: Node<T> | null = null) {}
 
-  insert(arr: T[]) {}
+  insert(arr: Array<T | null>) {
+    if (arr.length === 0) return;
+
+    let i = 0;
+
+    // create root
+    if (!this.root) {
+      if (arr[0] === 0) return;
+
+      let node = new Node(arr[0] as T);
+      this.root = node;
+      i++;
+
+      if (i === arr.length) return this;
+    }
+
+    // insert elements, use queue, similar to BFS
+    const queue = [this.root];
+    while (queue.length) {
+      let current = queue.shift();
+
+      // left
+      if (!current!.left) {
+        if (arr[i] !== null) {
+          let node = new Node(arr[i] as T);
+          current!.left = node;
+        }
+        i++;
+        if (i === arr.length) return this;
+      }
+
+      if (current?.left) {
+        queue.push(current.left);
+      }
+
+      // right
+      if (!current!.right) {
+        if (arr[i] !== null) {
+          let node = new Node(arr[i] as T);
+          current!.right = node;
+        }
+        i++;
+        if (i === arr.length) return this;
+      }
+
+      if (current?.right) {
+        queue.push(current.right);
+      }
+    }
+  }
 }
